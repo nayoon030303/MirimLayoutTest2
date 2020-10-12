53bebde08cf9689522f08d1cdf270a23c8e5bc5c
kny <kny030303>
2020-10-12 20:21:23 +0900

    java로 layout 작성 버튼 완성

--- app/src/main/java/kr/hs/emirim/s2019s05/mirimlayouttest2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019s05/mirimlayouttest2/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -13,25 +14,41 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
 
-        LinearLayout lineear1 = new LinearLayout(this);
+        final LinearLayout lineear1 = new LinearLayout(this);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                                                                             LinearLayout.LayoutParams.MATCH_PARENT);
         lineear1.setBackgroundColor(Color.rgb(181,178,255));
         lineear1.setOrientation(LinearLayout.VERTICAL);
 
-        EditText editText = new EditText(this);
-        lineear1.addView(editText);
-
+        editText = new EditText(this);
+        textView = new TextView(this);
         Button btn = new Button(this);
+        editText.setTextSize(20);
+        textView.setTextSize(20);
+        textView.setTextColor(Color.MAGENTA);
+
+        lineear1.addView(editText);
+        lineear1.addView(btn);
+        lineear1.addView(textView);
 
 
-        TextView textView = new TextView(this);
 
+        btn.setText("버튼입니다");
+        btn.setBackgroundColor(Color.YELLOW);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText.getText().toString()+"이/가 입력되었습니다.");
+            }
+        });
 
       /*  Button btn = new Button(this);
         btn.setText("클릭해 주세요");
